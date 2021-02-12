bbd462f80f0b491caf15f0cc7fd8c7ed88e33ba6
adam <adam@>
2021-02-12 14:22:20 +0200

    adf

--- php/node.php
+++ php/node.php
@@ -14,25 +14,12 @@ require_once "database.php";
 			$this->node_id=$node_id;
 		}
 	}
-	
-	class Current_Directory extends Node
+	class Directory_Node extends Node
 	{
-		/*an array of the dir_ids taken to reach here*/
-		public $path;
-
-		function __construct($user_id)
+		public $node_list;
+		/*the path in terms of the simulated filesystem*/
+		function __construct(string $abstract_path)
 		{
-			$this->dir_id=get_home_id($user_id);
-			$this->path=[$dir_id];
-		}
-		function change_directory($directory_id):bool
-		{
-			global $database;
-			if(!$database->is_directory($directory_id))
-			{
-				return false;
-			}
-			
 		}
 	}
 
